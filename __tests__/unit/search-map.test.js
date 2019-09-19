5ac6025fbc7060fc2eed3eceb14c11e5bd994c99
Lucas Rocha <lucasrocha7111@gmail.com>
2019-09-19 14:16:30 -0300

    Add more test for search map component

--- __tests__/unit/search-map.test.js
+++ __tests__/unit/search-map.test.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { SearchMap } from '../../src/screens/map/SearchMap'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+jest.useFakeTimers()
 
 
 describe('<SearchMap />', () => {
@@ -11,5 +17,12 @@ describe('<SearchMap />', () => {
     it('renders correctly', () => {
         const tree = renderer.create(<SearchMap />).toJSON()
         expect(tree).toMatchSnapshot()
+    })
+
+    it('on search function', async () => {
+        // _onSearch
+        const wrapper = shallow(<SearchMap />).instance()
+        await wrapper._getSearchResults('Aguas claras')
+        expect(wrapper.state.predictionList.length).toBeGreaterThan(0)
     })
 })
